b299688073d91db29a3511ae05f235b18e003802
kimhangyu <khk201@naver.com>
2024-09-10 00:05:10 +0900

    feat. Prodcut 개발
    -기본 기능 구현
    - models, serializers, urls, views
    - media 폴더 생성, settings.py 내 경로설정

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import (
+    ProductSerializer,
+    ProductDetailSerializer,
+)
+from .models import Product
 
-# Create your views here.
+
+class ProductListAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        Products = Product.objects.all()
+        serializer = ProductSerializer(Products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProductDetailAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get_object(self, pk):
+        return get_object_or_404(Product, pk=pk)
+
+    def get(self, request, pk):
+        Product = self.get_object(pk)
+        serializer = ProductDetailSerializer(Product)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        Product = self.get_object(pk)
+        serializer = ProductDetailSerializer(
+            Product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    def delete(self, request, pk):
+        Product = self.get_object(pk)
+        Product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
