049d1814dc1bbf686875e1b44b09dca039c1c1ec
kimhangyu <khk201@naver.com>
2024-09-09 22:53:45 +0900

    feat. app 세팅
    -products, accounts app 생성
    -urls.py 생성
    -setting.py 내 app추가
    -config.py 시크릿키 이동

--- spartamarket_drf/settings.py
+++ spartamarket_drf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+**=&skslk=^2s!z^(h@ef!m_-5^9gi(n!$h=)f!odane&ngxi'
+SECRET_KEY = config.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party apps
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    'django_extensions',
+    # Local apps
+    'products',
+    'accounts',
 ]
 
 MIDDLEWARE = [
